9dedb822aedeeb35d1725b5547b7c63f06659768
NeilYang <cloudyang3759@gmail.com>
2022-07-05 20:34:56 +0800

    07052034

---
+++ Front/wwwroot/js/Layout.js
@@ -0,0 +1,25 @@
+﻿
+var navBar = document.getElementById("navBar");
+
+function togglebtn() {
+    navBar.classList.toggle("hidemenu");
+}
+
+var Today = new Date();
+var t2 = new Date();
+t2.setHours(t2.getHours() + 2);
+var y = Today.getFullYear();
+let m = String(Today.getMonth() + 1).padStart(2, '0');
+let d = String(Today.getDate()).padStart(2, '0');
+var h = Today.getHours();
+var h1 = t2.getHours();
+var min = Today.getMinutes();
+var CheckInTime = y + "-" + m + "-" + d + "T" + h + ":" + min;
+var CheckOutTime = y + "-" + m + "-" + d + "T" + h1 + ":" + min;
+
+document.querySelector('#checkin-time').setAttribute('value', CheckInTime)
+document.querySelector('#checkout-time').setAttribute('value', CheckOutTime)
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
