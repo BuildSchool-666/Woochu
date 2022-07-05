749388a867552a9b214f1e943777a80cedc43a2f
YongChyi <samy19990103@gmail.com>
2022-07-05 18:52:28 +0800

    07051852

--- Front/Controllers/Account-settingsController.cs
+++ Front/Controllers/Account-settingsController.cs
@@ -8,6 +8,26 @@ namespace Front.Controllers
         {
             return View();
         }
+        public IActionResult PersonalInformation()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult GlobalPreference()
+        {
+            return View();
+        }
+        public IActionResult GuesrReferrals()
+        {
+            return View();
+        }
+        public IActionResult ProfessionalHostTools()
+        {
+            return View();
+        }
 
     }
 }
