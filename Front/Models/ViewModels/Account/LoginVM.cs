b2afcdfab7db68158ffc120dbb5984a26bbb483e
YongChyi <samy19990103@gmail.com>
2022-08-03 19:27:29 +0800

    a

--- Front/Models/ViewModels/Account/LoginVM.cs
+++ Front/Models/ViewModels/Account/LoginVM.cs
@@ -8,9 +8,8 @@ namespace Front.Models.ViewModels.Account
         [Required(ErrorMessage ="要填喔")]
         [EmailAddress]
         public string Email { get; set; }
-        [Required]
+        [Required(ErrorMessage = "要填喔")]
         [DataType(DataType.Password)]
-        [StringLength(8,ErrorMessage = "沒機會顯示")]
         public string Password { get; set; }
 
     }
