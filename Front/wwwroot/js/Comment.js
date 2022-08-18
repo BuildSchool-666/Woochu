78c9807f548cb93e83665e86bd889d62d969040e
jamesweiii <a1995james@gmail.com>
2022-08-19 00:10:18 +0800

    65

--- Front/wwwroot/js/Comment.js
+++ Front/wwwroot/js/Comment.js
@@ -27,23 +27,7 @@ function InsertComment(roomId,index,orderid) {
     const webapiUrl = "/api/CommentApi/";
 
 
-    //let request = new Request(webapiUrl + "Delete", {
-    //    method: "DELETE",
-    //    headers: {
-    //        "content-type": "application/json; charset=UTF-8"
-    //    },
-    //    body: JSON.stringify(roomId)
-    //});
-    //fetch(request)
-    //    .then(response => {
-    //        if (response.ok) {
-    //            return response.text();
-    //        }
-    //        else {
-    //            throw new Error(`發生錯誤:${response.status},${response.statusText}`);
-    //        }
-    //    });
-    
+
     let score = document.querySelector('input[name="inlineRadioOptions"]:checked').value;
     let text = $(`.text_${index}`).val();
     //var html = $("#validationServer01").val();
